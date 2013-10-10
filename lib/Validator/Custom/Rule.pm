ef1a0c59bcdc89ba28231e0cc8301f77e9b58205
Yuki Kimoto <kimoto.yuki@gmail.com>
2013-10-10 15:35:50 +0900

    add Validator::Custom::Rule document

--- lib/Validator/Custom/Rule.pm
+++ lib/Validator/Custom/Rule.pm
@@ -57,6 +57,55 @@ sub parse {
   }
   
   $self->rule($normalized_rule);
+  
+  return $self;
 }
 
 1;
+
+=head1 NAME
+
+Validator::Custom::Rule - Rule object
+
+=head1 SYNOPSYS
+  
+  # Create rule object and parse rule
+  my $rule_obj = Validator::Custom::Rule->new;
+  my $rule = [
+    id => [
+      'ascii'
+    ],
+    name => [
+      'not_blank'
+    ]
+  ]
+  $rule_obj->parse($rule);
+  
+  # Pass rule object to validate method
+  my $vc = Validator::Custom->new;
+  my $data = {id => '001', name => 'kimoto'};
+  my $result = $vc->validate($data, $rule_obj);
+
+  # Usual way is shortcut of above
+  my $result2 = $vc->validate($date, $rule);
+  
+=head1 DESCRIPTION
+
+Validator::Custom::Rule is the class to parse rule and store it as object.
+
+=head1 ATTRIBUTES
+
+=head2 rule
+
+  my $content = $rule_obj->rule;
+  $rule_obj = $rule->rule($content);
+
+Content of rule object.
+
+=head1 METHODS
+
+=head2 parse
+
+  $rule_obj = $rule_obj->parse($rule);
+
+Parse rule and store it to C<rule> attribute.
